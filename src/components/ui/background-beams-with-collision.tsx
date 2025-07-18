83faf724aad403a7f8723cbb5386925441971e10
alvaro <alvarososafrancisco1432@gmail.com>
2025-07-18 00:43:16 +0000

    fix error background beams

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -127,15 +127,6 @@ const CollisionMechanism = React.forwardRef<
   });
   const [beamKey, setBeamKey] = useState(0);
   const [cycleCollisionDetected, setCycleCollisionDetected] = useState(false);
-  const [windowHeight, setWindowHeight] = useState(0);
-
-  useEffect(() => {
-    // Actualizar altura al cargar y al cambiar tamaño
-    const updateHeight = () => setWindowHeight(window.innerHeight);
-    updateHeight();
-    window.addEventListener("resize", updateHeight);
-    return () => window.removeEventListener("resize", updateHeight);
-  }, []);
 
   useEffect(() => {
     const checkCollision = () => {
